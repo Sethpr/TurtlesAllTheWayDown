7e3eed4e4c63c52755f081715131290074c1f68c
seth <sprentice97@gmail.com>
2023-04-02 12:20:30 -0400

    did factorial, shaved off a whole line from how i usually see it, woohoo

--- src/main/java/Turtles.java
+++ src/main/java/Turtles.java
@@ -1,7 +1,17 @@
-package PACKAGE_NAME;
-
 /**
  * Created by kristofer on 7/14/20.
  */
 public class Turtles {
+
+    public static int factorialR(int num){
+        return (num == 1) ? 1 : num * factorialR(num-1);
+    }
+
+    public static int factorial(int num){
+        int ans = 1;
+        for (int i = 1; i <= num; i++) {
+            ans*=i;
+        }
+        return ans;
+    }
 }
--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,8 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class TurtlesTest {
+
+    @Test
+    public void factorialTestRecursive() {
+        Assert.assertEquals(1, Turtles.factorialR(1));
+        Assert.assertEquals(120, Turtles.factorialR(5));
+        Assert.assertEquals(479001600, Turtles.factorialR(12));
+    }
+
+    @Test
+    public void factorialTestLoop() {
+        Assert.assertEquals(1, Turtles.factorial(1));
+        Assert.assertEquals(120, Turtles.factorial(5));
+        Assert.assertEquals(479001600, Turtles.factorial(12));
+    }
+
 
 }
