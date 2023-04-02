a73af189df9e35a2abca5e7c228a9c76c48d709b
seth <sprentice97@gmail.com>
2023-04-02 17:49:24 -0400

    completed gcd and timed, i think

--- src/main/java/Turtles.java
+++ src/main/java/Turtles.java
@@ -14,4 +14,18 @@ public class Turtles {
         }
         return ans;
     }
+
+
+    public static int gcdR(int larger, int smaller) {
+        return (smaller == 0) ? larger : gcdR(smaller, larger % smaller);
+    }
+
+    public static int gcd(int larger, int smaller) {
+        for (int i = smaller; i > 1 ; i--) {
+            if(larger % i == 0 && smaller % i == 0){
+                return i;
+            }
+        }
+        return 1;
+    }
 }
--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +25,53 @@ public class TurtlesTest {
         Assert.assertEquals(479001600, Turtles.factorial(12));
     }
 
+    @Test
+    public void gcdTestR() {
+        Assert.assertEquals(2, Turtles.gcdR(4,2));
+        Assert.assertEquals(24, Turtles.gcdR(1440, 408));
+        Assert.assertEquals(1, Turtles.gcdR(89, 7));
+    }
 
+    @Test
+    public void gcdTest() {
+        Assert.assertEquals(2, Turtles.gcd(4,2));
+        Assert.assertEquals(24, Turtles.gcd(1440, 408));
+        Assert.assertEquals(1, Turtles.gcd(89, 7));
+    }
+
+    @Test
+    public void testSpeed() throws InterruptedException {
+        Instant start;
+        Instant stop;
+
+        start = Instant.now();
+        Turtles.gcd(1440, 408);
+        stop = Instant.now();
+
+        System.out.println("Non recursive with 1440 and 408: " + Duration.between(start, stop).toMillis());
+
+        //Thread.sleep(500);
+
+        start = Instant.now();
+        Turtles.gcdR(1440, 408);
+        stop = Instant.now();
+
+        System.out.println("Recursive with 1440 and 408: " + Duration.between(start, stop).toMillis());
+
+        //Thread.sleep(500);
+
+        start = Instant.now();
+        Turtles.gcd(74687, 41911);
+        stop = Instant.now();
+
+        System.out.println("Non recursive with 74687 and 41911: " + Duration.between(start, stop).toMillis());
+
+        //Thread.sleep(500);
+
+        start = Instant.now();
+        Turtles.gcdR(74687, 41911);
+        stop = Instant.now();
+
+        System.out.println("Recursive with 74687 and 41911: " + Duration.between(start, stop).toMillis());
+    }
 }
